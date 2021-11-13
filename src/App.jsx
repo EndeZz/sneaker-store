3f51fc94e1305de859452719fef5b396557d828e
endezz <r-serega13@yandex.ru>
2021-11-13 21:33:16 +0400

    feat: create react app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './styles/index.scss';
+import React from 'react';
+
+const App = () => {
+  console.log('hi');
+
+  return (
+    <div>
+      <div>Template</div>
+    </div>
+  );
+};
+
+export default App;
